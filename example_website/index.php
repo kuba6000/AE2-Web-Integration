99dd783d274d7869c5773bfe046768156112b8a9
kuba6000 <kuba.123123.6000@gmail.com>
2024-09-03 00:35:26 +0200

    Add favicon

--- example_website/index.php
+++ example_website/index.php
@@ -39,6 +39,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>AE2</title>
