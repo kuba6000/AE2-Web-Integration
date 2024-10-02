54104d4195cb651a1991f8ec474f4ad8b6db2734
Jakub <kuba.123123.6000@gmail.com>
2024-10-03 01:00:05 +0200

    -

--- src/main/java/com/kuba6000/ae2webintegration/mixins/MixinPlugin.java
+++ src/main/java/com/kuba6000/ae2webintegration/mixins/MixinPlugin.java
@@ -46,27 +46,6 @@ public class MixinPlugin implements IMixinConfigPlugin {
                 "AE2.CraftingCPUClusterMixin",
                 "AE2.CraftingCPUClusterAccessor"));
 
-        // byte[] bytes = null;
-        // try {
-        // bytes = Launch.classLoader.getClassBytes("appeng.me.cluster.implementations.CraftingCPUCluster");
-        // } catch (IOException e) {
-        // throw new RuntimeException(e);
-        // }
-        //
-        // ClassNode cn = new ClassNode();
-        // ClassReader cr = new ClassReader(bytes);
-        // cr.accept(cn, 0);
-        //
-        // if (cn.methods.stream()
-        // .anyMatch(m -> m.name.equals("mergeJob"))) {
-        // LOG.warn("Job merging detected, completely disabling it!!!!!!!!!!!!!");
-        // mixins.addAll(
-        // Arrays.asList(
-        // "AE2.MergeDisabler.CraftingCPUClusterMixin",
-        // "AE2.MergeDisabler.ContainerCraftConfirmMixin",
-        // "AE2.MergeDisabler.CraftingGridCacheMixin"));
-        // }
-
         LOG.info("MIXING INTO AE2 LETS GOOOOOOOOOOOOOOOOOOOOOOOOO");
 
         return mixins;
