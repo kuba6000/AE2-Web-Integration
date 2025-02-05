4f681a48ce68fd07130967f2d8716ee7c45dec56
Jakub <53441451+kuba6000@users.noreply.github.com>
2025-02-05 12:43:53 +0100

    Add interface location to interface chart (#12)
    * Add interface location to interface chart
    
    * Update HTML
    
    * Fix NPE

--- src/main/java/com/kuba6000/ae2webintegration/ae2interface/implementations/PatternProviderViewable.java
+++ src/main/java/com/kuba6000/ae2webintegration/ae2interface/implementations/PatternProviderViewable.java
@@ -1,7 +1,9 @@
 package com.kuba6000.ae2webintegration.ae2interface.implementations;
 
+import com.kuba6000.ae2webintegration.core.api.DimensionalCoords;
 import com.kuba6000.ae2webintegration.core.interfaces.IPatternProviderViewable;
 
+import appeng.api.util.DimensionalCoord;
 import appeng.api.util.IInterfaceViewable;
 
 public class PatternProviderViewable extends IAEStrongObject<IInterfaceViewable> implements IPatternProviderViewable {
@@ -13,5 +15,11 @@ public class PatternProviderViewable extends IAEStrongObject<IInterfaceViewable>
     @Override
     public String getName() {
         return object.getName();
+    }
+
+    @Override
+    public DimensionalCoords getLocation() {
+        DimensionalCoord coord = get().getLocation();
+        return new DimensionalCoords(coord.getDimension(), coord.x, coord.y, coord.z);
     }
 }
