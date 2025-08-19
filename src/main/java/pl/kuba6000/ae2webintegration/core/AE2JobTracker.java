96e67cf21a34415394b39cb463b2cb5c89045a32
Jakub <kuba.123123.6000@gmail.com>
2025-08-19 02:58:36 +0200

    Make sure CPU has id before sending discord message

--- src/main/java/pl/kuba6000/ae2webintegration/core/AE2JobTracker.java
+++ src/main/java/pl/kuba6000/ae2webintegration/core/AE2JobTracker.java
@@ -208,6 +208,8 @@ public class AE2JobTracker {
         if (!Config.AE_PUBLIC_MODE && !Config.DISCORD_WEBHOOK.isEmpty()) {
             IAESecurityGrid securityGrid = grid.web$getSecurityGrid();
             if (securityGrid != null && securityGrid.web$isAvailable()) {
+                IAECraftingGrid craftingGrid = grid.web$getCraftingGrid();
+                craftingGrid.web$getCPUs(); // make sure the cpu has id
                 DiscordManager.postMessageNonBlocking(
                     new DiscordManager.DiscordEmbed(
                         "AE2 Job Tracker [ Grid " + securityGrid.web$getSecurityKey()
