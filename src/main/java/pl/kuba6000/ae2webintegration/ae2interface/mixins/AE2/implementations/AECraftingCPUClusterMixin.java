PantACRO4life/AE2-Web-Integration2

Basic support for AdvancedAE (#43)

src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/AE2/implementations/AECraftingCPUClusterMixin.java
@@ -1,110 +1,111 @@
 package pl.kuba6000.ae2webintegration.ae2interface.mixins.AE2.implementations;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import net.pedroksl.advanced_ae.common.cluster.AdvCraftingCPU;
 
+import org.spongepowered.asm.mixin.Mixin;
+
+import appeng.api.networking.crafting.ICraftingCPU;
 import appeng.api.stacks.AEKey;
 import appeng.api.stacks.KeyCounter;
 import appeng.me.cluster.implementations.CraftingCPUCluster;
 import pl.kuba6000.ae2webintegration.ae2interface.accessors.ICraftingCPULogicAccessor;
+import pl.kuba6000.ae2webintegration.ae2interface.implementations.AE;
 import pl.kuba6000.ae2webintegration.core.interfaces.IAEGenericStack;
 import pl.kuba6000.ae2webintegration.core.interfaces.IAEKey;
 import pl.kuba6000.ae2webintegration.core.interfaces.ICraftingCPUCluster;
 import pl.kuba6000.ae2webintegration.core.interfaces.IItemList;
 
-@Mixin(value = CraftingCPUCluster.class, remap = false)
-public abstract class AECraftingCPUClusterMixin implements ICraftingCPUCluster {
-
-    @Unique
-    private int web$internalID = -1;
+@Mixin(value = ICraftingCPU.class, remap = false)
+public interface AECraftingCPUClusterMixin extends ICraftingCPUCluster {
 
     @Override
-    public void web$setInternalID(int id) {
-        web$internalID = id;
+    default public void web$setInternalID(int id) {
+        AE.cpuInternalIDMap.put(this, id);
     }
 
     @Override
-    public boolean web$hasCustomName() {
-        return !(((CraftingCPUCluster) (Object) this).getName() == null);
+    default public boolean web$hasCustomName() {
+        return !(((ICraftingCPU) this).getName() == null);
     }
 
     @Override
-    public String web$getName() {
-        return web$hasCustomName() ? ((CraftingCPUCluster) (Object) this).getName()
-            .getString() : ("CPU #" + web$internalID);
+    default public String web$getName() {
+        return web$hasCustomName() ? ((ICraftingCPU) this).getName()
+            .getString() : ("CPU #" + AE.cpuInternalIDMap.getOrDefault(this, -1));
     }
 
     @Override
-    public long web$getAvailableStorage() {
-        return ((CraftingCPUCluster) (Object) this).getAvailableStorage();
+    default public long web$getAvailableStorage() {
+        return ((ICraftingCPU) this).getAvailableStorage();
     }
 
-    @Unique
-    private boolean web$isUsedStorageAvailable = true;
-
-    @Unique
-    private boolean web$usedStorageInitialized = false;
-
     @Override
-    public long web$getUsedStorage() {
+    default public long web$getUsedStorage() {
         return -1L;
-        // if (!web$usedStorageInitialized) {
-        // web$usedStorageInitialized = true;
-        // try {
-        // appeng.me.cluster.implementations.CraftingCPUCluster.class.getDeclaredMethod("getUsedStorage");
-        // } catch (NoSuchMethodException e) {
-        // web$isUsedStorageAvailable = false;
-        // return -1L;
-        // }
-        // }
-        // if (!web$isUsedStorageAvailable) return -1L;
-        // return ((CraftingCPUCluster) (Object) this).getUsedStorage();
     }
 
     @Override
-    public long web$getCoProcessors() {
-        return ((CraftingCPUCluster) (Object) this).getCoProcessors();
+    default public long web$getCoProcessors() {
+        return ((ICraftingCPU) this).getCoProcessors();
     }
 
     @Override
-    public boolean web$isBusy() {
-        return ((CraftingCPUCluster) (Object) this).isBusy();
+    default public boolean web$isBusy() {
+        return ((ICraftingCPU) this).isBusy();
     }
 
     @Override
-    public void web$cancel() {
-        ((CraftingCPUCluster) (Object) this).cancelJob();
+    default public void web$cancel() {
+        ((ICraftingCPU) this).cancelJob();
     }
 
     @Override
-    public IAEGenericStack web$getFinalOutput() {
-        return (IAEGenericStack) (Object) ((CraftingCPUCluster) (Object) this).craftingLogic.getFinalJobOutput();
+    default public IAEGenericStack web$getFinalOutput() {
+        if (web$isBusy()) return (IAEGenericStack) (Object) ((ICraftingCPU) this).getJobStatus()
+            .crafting();
+        return null;
     }
 
     @Override
-    public void web$getAllItems(IItemList list) {
-        ((CraftingCPUCluster) (Object) this).craftingLogic.getAllItems((KeyCounter) (Object) list);
+    default public void web$getAllItems(IItemList list) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            cpuCluster.craftingLogic.getAllItems((KeyCounter) (Object) list);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu)
+            advCpu.craftingLogic.getAllItems((KeyCounter) (Object) list);
     }
 
     @Override
-    public long web$getActiveItems(IAEKey key) {
-        return ((CraftingCPUCluster) (Object) this).craftingLogic.getWaitingFor((AEKey) key);
+    default public long web$getActiveItems(IAEKey key) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return cpuCluster.craftingLogic.getWaitingFor((AEKey) key);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu) return advCpu.craftingLogic.getWaitingFor((AEKey) key);
+        return 0L;
     }
 
     @Override
-    public long web$getPendingItems(IAEKey key) {
-        return ((CraftingCPUCluster) (Object) this).craftingLogic.getPendingOutputs((AEKey) key);
+    default public long web$getPendingItems(IAEKey key) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return cpuCluster.craftingLogic.getPendingOutputs((AEKey) key);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu) return advCpu.craftingLogic.getWaitingFor((AEKey) key);
+        return 0L;
     }
 
     @Override
-    public long web$getStorageItems(IAEKey key) {
-        return ((CraftingCPUCluster) (Object) this).craftingLogic.getStored((AEKey) key);
+    default public long web$getStorageItems(IAEKey key) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return cpuCluster.craftingLogic.getStored((AEKey) key);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu) return advCpu.craftingLogic.getWaitingFor((AEKey) key);
+        return 0L;
     }
 
     @Override
-    public IItemList web$getWaitingFor() {
-        return (IItemList) (Object) ((ICraftingCPULogicAccessor) ((CraftingCPUCluster) (Object) this).craftingLogic)
-            .web$getJob()
-            .web$getWaitingFor().list;
+    default public IItemList web$getWaitingFor() {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return (IItemList) (Object) ((ICraftingCPULogicAccessor) cpuCluster.craftingLogic).web$getJob()
+                .web$getWaitingFor().list;
+        else if ((Object) this instanceof AdvCraftingCPU advCpu)
+            return (IItemList) (Object) ((ICraftingCPULogicAccessor) advCpu.craftingLogic).web$getJob()
+                .web$getWaitingFor().list;
+        return (IItemList) (Object) new KeyCounter();
     }
 }

kuba6000/AE2-Web-Integration

src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/AE2/implementations/AECraftingCPUClusterMixin.java
@@ -1,110 +1,111 @@
 package pl.kuba6000.ae2webintegration.ae2interface.mixins.AE2.implementations;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import net.pedroksl.advanced_ae.common.cluster.AdvCraftingCPU;
 
+import org.spongepowered.asm.mixin.Mixin;
+
+import appeng.api.networking.crafting.ICraftingCPU;
 import appeng.api.stacks.AEKey;
 import appeng.api.stacks.KeyCounter;
 import appeng.me.cluster.implementations.CraftingCPUCluster;
 import pl.kuba6000.ae2webintegration.ae2interface.accessors.ICraftingCPULogicAccessor;
+import pl.kuba6000.ae2webintegration.ae2interface.implementations.AE;
 import pl.kuba6000.ae2webintegration.core.interfaces.IAEGenericStack;
 import pl.kuba6000.ae2webintegration.core.interfaces.IAEKey;
 import pl.kuba6000.ae2webintegration.core.interfaces.ICraftingCPUCluster;
 import pl.kuba6000.ae2webintegration.core.interfaces.IItemList;
 
-@Mixin(value = CraftingCPUCluster.class, remap = false)
-public abstract class AECraftingCPUClusterMixin implements ICraftingCPUCluster {
-
-    @Unique
-    private int web$internalID = -1;
+@Mixin(value = ICraftingCPU.class, remap = false)
+public interface AECraftingCPUClusterMixin extends ICraftingCPUCluster {
 
     @Override
-    public void web$setInternalID(int id) {
-        web$internalID = id;
+    default public void web$setInternalID(int id) {
+        AE.cpuInternalIDMap.put(this, id);
     }
 
     @Override
-    public boolean web$hasCustomName() {
-        return !(((CraftingCPUCluster) (Object) this).getName() == null);
+    default public boolean web$hasCustomName() {
+        return !(((ICraftingCPU) this).getName() == null);
     }
 
     @Override
-    public String web$getName() {
-        return web$hasCustomName() ? ((CraftingCPUCluster) (Object) this).getName()
-            .getString() : ("CPU #" + web$internalID);
+    default public String web$getName() {
+        return web$hasCustomName() ? ((ICraftingCPU) this).getName()
+            .getString() : ("CPU #" + AE.cpuInternalIDMap.getOrDefault(this, -1));
     }
 
     @Override
-    public long web$getAvailableStorage() {
-        return ((CraftingCPUCluster) (Object) this).getAvailableStorage();
+    default public long web$getAvailableStorage() {
+        return ((ICraftingCPU) this).getAvailableStorage();
     }
 
-    @Unique
-    private boolean web$isUsedStorageAvailable = true;
-
-    @Unique
-    private boolean web$usedStorageInitialized = false;
-
     @Override
-    public long web$getUsedStorage() {
+    default public long web$getUsedStorage() {
         return -1L;
-        // if (!web$usedStorageInitialized) {
-        // web$usedStorageInitialized = true;
-        // try {
-        // appeng.me.cluster.implementations.CraftingCPUCluster.class.getDeclaredMethod("getUsedStorage");
-        // } catch (NoSuchMethodException e) {
-        // web$isUsedStorageAvailable = false;
-        // return -1L;
-        // }
-        // }
-        // if (!web$isUsedStorageAvailable) return -1L;
-        // return ((CraftingCPUCluster) (Object) this).getUsedStorage();
     }
 
     @Override
-    public long web$getCoProcessors() {
-        return ((CraftingCPUCluster) (Object) this).getCoProcessors();
+    default public long web$getCoProcessors() {
+        return ((ICraftingCPU) this).getCoProcessors();
     }
 
     @Override
-    public boolean web$isBusy() {
-        return ((CraftingCPUCluster) (Object) this).isBusy();
+    default public boolean web$isBusy() {
+        return ((ICraftingCPU) this).isBusy();
     }
 
     @Override
-    public void web$cancel() {
-        ((CraftingCPUCluster) (Object) this).cancel();
+    default public void web$cancel() {
+        ((ICraftingCPU) this).cancel();
     }
 
     @Override
-    public IAEGenericStack web$getFinalOutput() {
-        return (IAEGenericStack) (Object) ((CraftingCPUCluster) (Object) this).craftingLogic.getFinalJobOutput();
+    default public IAEGenericStack web$getFinalOutput() {
+        if (web$isBusy()) return (IAEGenericStack) (Object) ((ICraftingCPU) this).getJobStatus()
+            .crafting();
+        return null;
     }
 
     @Override
-    public void web$getAllItems(IItemList list) {
-        ((CraftingCPUCluster) (Object) this).craftingLogic.getAllItems((KeyCounter) (Object) list);
+    default public void web$getAllItems(IItemList list) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            cpuCluster.craftingLogic.getAllItems((KeyCounter) (Object) list);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu)
+            advCpu.craftingLogic.getAllItems((KeyCounter) (Object) list);
     }
 
     @Override
-    public long web$getActiveItems(IAEKey key) {
-        return ((CraftingCPUCluster) (Object) this).craftingLogic.getWaitingFor((AEKey) key);
+    default public long web$getActiveItems(IAEKey key) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return cpuCluster.craftingLogic.getWaitingFor((AEKey) key);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu) return advCpu.craftingLogic.getWaitingFor((AEKey) key);
+        return 0L;
     }
 
     @Override
-    public long web$getPendingItems(IAEKey key) {
-        return ((CraftingCPUCluster) (Object) this).craftingLogic.getPendingOutputs((AEKey) key);
+    default public long web$getPendingItems(IAEKey key) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return cpuCluster.craftingLogic.getPendingOutputs((AEKey) key);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu) return advCpu.craftingLogic.getWaitingFor((AEKey) key);
+        return 0L;
     }
 
     @Override
-    public long web$getStorageItems(IAEKey key) {
-        return ((CraftingCPUCluster) (Object) this).craftingLogic.getStored((AEKey) key);
+    default public long web$getStorageItems(IAEKey key) {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return cpuCluster.craftingLogic.getStored((AEKey) key);
+        else if ((Object) this instanceof AdvCraftingCPU advCpu) return advCpu.craftingLogic.getWaitingFor((AEKey) key);
+        return 0L;
     }
 
     @Override
-    public IItemList web$getWaitingFor() {
-        return (IItemList) (Object) ((ICraftingCPULogicAccessor) ((CraftingCPUCluster) (Object) this).craftingLogic)
-            .web$getJob()
-            .web$getWaitingFor().list;
+    default public IItemList web$getWaitingFor() {
+        if ((Object) this instanceof CraftingCPUCluster cpuCluster)
+            return (IItemList) (Object) ((ICraftingCPULogicAccessor) cpuCluster.craftingLogic).web$getJob()
+                .web$getWaitingFor().list;
+        else if ((Object) this instanceof AdvCraftingCPU advCpu)
+            return (IItemList) (Object) ((ICraftingCPULogicAccessor) advCpu.craftingLogic).web$getJob()
+                .web$getWaitingFor().list;
+        return (IItemList) (Object) new KeyCounter();
     }
 }