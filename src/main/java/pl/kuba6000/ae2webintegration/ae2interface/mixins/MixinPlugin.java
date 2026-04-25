2d31bf6c9eae477d177643797af2d4eb3dee813e
kuba6000 <kuba.123123.6000@gmail.com>
2026-04-25 10:45:54 +0200

    Update MixinPlugin.java

--- src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPlugin.java
+++ src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPlugin.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import net.minecraftforge.fml.loading.FMLLoader;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
