910366636bcff009b6edfe7659e7f27d7d59611a
kuba6000 <kuba.123123.6000@gmail.com>
2026-05-15 03:29:15 +0200

    spotlessApply

--- src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPlugin.java
+++ src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPlugin.java
@@ -29,7 +29,7 @@ public class MixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return false;
+        return true;
     }
 
     @Override
