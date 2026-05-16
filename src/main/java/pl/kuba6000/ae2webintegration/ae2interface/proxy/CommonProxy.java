9534693f71dadab33422f76827090501223868b7
kuba6000 <kuba.123123.6000@gmail.com>
2026-05-16 15:34:22 +0200

    Set VERSION_IDENTIFIER to -forge-1.20.1 in CommonProxy preInit

--- src/main/java/pl/kuba6000/ae2webintegration/ae2interface/proxy/CommonProxy.java
+++ src/main/java/pl/kuba6000/ae2webintegration/ae2interface/proxy/CommonProxy.java
@@ -8,6 +8,7 @@ import pl.kuba6000.ae2webintegration.core.GridData;
 import pl.kuba6000.ae2webintegration.core.StartupHandler;
 import pl.kuba6000.ae2webintegration.core.WebData;
 import pl.kuba6000.ae2webintegration.core.WebEngine;
+import pl.kuba6000.ae2webintegration.core.utils.VersionChecker;
 
 /**
  * Lifecycle coordinator for AE2 Web Integration.
@@ -15,6 +16,7 @@ import pl.kuba6000.ae2webintegration.core.WebEngine;
 public class CommonProxy {
 
     public void preInit(Platform platform, String version) {
+        VersionChecker.setVersionIdentifier("-forge-1.20.1");
         WebEngine.init(platform, version);
         Config.init(platform.getConfigDirectory());
         WebData.loadData();
