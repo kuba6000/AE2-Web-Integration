185f3a9e88ce8eddb0f748166b9ebfba3ca59db6
kuba6000 <kuba.123123.6000@gmail.com>
2026-07-07 15:35:49 +0200

    Harden GTNH mixin loading

--- src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPlugin.java
+++ src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPlugin.java
@@ -28,7 +28,7 @@ public class MixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return false;
+        return true;
     }
 
     @Override
