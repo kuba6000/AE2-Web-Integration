80889507b5c98e14ccdb69ec45c60d5c756f75e2
kuba6000 <kuba.123123.6000@gmail.com>
2026-07-07 16:01:51 +0200

    Apply production build formatting

--- src/test/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPluginTest.java
+++ src/test/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/MixinPluginTest.java
@@ -1,8 +1,8 @@
 package pl.kuba6000.ae2webintegration.ae2interface.mixins;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class MixinPluginTest {
 
