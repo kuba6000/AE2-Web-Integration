6306141ad9d0ff72e2f6c9dd23ad11cef46e533c
kuba6000 <kuba.123123.6000@gmail.com>
2026-07-17 17:30:03 +0200

    Rename web engine core API

--- src/main/java/pl/kuba6000/ae2webintegration/ae2interface/proxy/CommonProxy.java
+++ src/main/java/pl/kuba6000/ae2webintegration/ae2interface/proxy/CommonProxy.java
@@ -2,7 +2,7 @@ package pl.kuba6000.ae2webintegration.ae2interface.proxy;
 
 import pl.kuba6000.ae2webintegration.ae2interface.AE2WebIntegration;
 import pl.kuba6000.ae2webintegration.ae2interface.platform.Platform;
-import pl.kuba6000.ae2webintegration.core.WebEngine;
+import pl.kuba6000.ae2webintegration.core.CoreEngine;
 
 /**
  * Lifecycle coordinator for AE2 Web Integration.
@@ -10,15 +10,15 @@ import pl.kuba6000.ae2webintegration.core.WebEngine;
 public class CommonProxy {
 
     public void preInit(Platform platform, String version) {
-        WebEngine.init(platform, version, "-forge-1.20.1");
+        CoreEngine.init(platform, version, "-forge-1.20.1");
         AE2WebIntegration.LOG.info("AE2WebIntegration loading at version {}", version);
     }
 
     public void onServerStarted() {
-        WebEngine.onServerStarted();
+        CoreEngine.onServerStarted();
     }
 
     public void onServerStopping() {
-        WebEngine.onServerStopping();
+        CoreEngine.onServerStopping();
     }
 }
