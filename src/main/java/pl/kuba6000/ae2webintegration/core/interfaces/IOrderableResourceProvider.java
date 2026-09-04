098261c3f9d43ca292a788733d9835de4b7e9150
kuba6000 <kuba.123123.6000@gmail.com>
2026-09-04 23:02:30 +0200

    Remove recipe membership infrastructure from stable identity

--- src/main/java/pl/kuba6000/ae2webintegration/core/interfaces/IOrderableResourceProvider.java
+++
@@ -1,9 +0,0 @@
-package pl.kuba6000.ae2webintegration.core.interfaces;
-
-import pl.kuba6000.ae2webintegration.core.identity.OrderableResources;
-
-/** Native grid/service capability for precise current ordering, distinct from storage listing. */
-public interface IOrderableResourceProvider {
-
-    OrderableResources web$getOrderableResources();
-}
