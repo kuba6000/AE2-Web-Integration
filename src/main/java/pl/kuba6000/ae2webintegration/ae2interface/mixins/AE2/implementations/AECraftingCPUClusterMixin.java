e9f0f013010be704a494c111b6d0bd856b7a7bfd
kuba6000 <kuba.123123.6000@gmail.com>
2026-09-08 17:16:39 +0200

    Hash raw CPU identity data on 1.12.2

--- src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/AE2/implementations/AECraftingCPUClusterMixin.java
+++ src/main/java/pl/kuba6000/ae2webintegration/ae2interface/mixins/AE2/implementations/AECraftingCPUClusterMixin.java
@@ -58,13 +58,13 @@ public abstract class AECraftingCPUClusterMixin implements ICraftingCPUCluster {
     @Override
     public @NotNull StableKey web$getId() {
         if (web$stableKey == null) {
-            web$stableKey = StableKey.create(sink -> {
-                StableKey.writeText(sink, "cpu:ae2");
-                StableKey.writeText(sink, Integer.toString(getWorld().provider.getDimension()));
-                sink.putInt(min.x)
-                    .putInt(min.y)
-                    .putInt(min.z);
-            });
+            web$stableKey = StableKey.create(
+                sink -> {
+                    sink.putInt(getWorld().provider.getDimension())
+                        .putInt(min.x)
+                        .putInt(min.y)
+                        .putInt(min.z);
+                });
         }
         return web$stableKey;
     }
