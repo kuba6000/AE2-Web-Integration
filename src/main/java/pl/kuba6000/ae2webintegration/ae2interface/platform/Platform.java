f9ad4a365f5f07348757efb90bfef7a27f8e83b4
Jakub <kuba.123123.6000@gmail.com>
2026-09-22 01:03:22 +0200

    rename to old config

--- src/main/java/pl/kuba6000/ae2webintegration/ae2interface/platform/Platform.java
+++ src/main/java/pl/kuba6000/ae2webintegration/ae2interface/platform/Platform.java
@@ -1,7 +1,6 @@
 package pl.kuba6000.ae2webintegration.ae2interface.platform;
 
 import java.io.File;
-import java.util.Map;
 import java.util.UUID;
 
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -10,6 +9,7 @@ import com.github.bsideup.jabel.Desugar;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import pl.kuba6000.ae2webintegration.ae2interface.config.LegacyConfigReader;
+import pl.kuba6000.ae2webintegration.core.api.ILegacyConfigProvider;
 import pl.kuba6000.ae2webintegration.core.api.IServerPlatform;
 
 @Desugar
@@ -43,7 +43,7 @@ public record Platform(File configDir) implements IServerPlatform {
     }
 
     @Override
-    public Map<String, Object> readLegacyConfig() {
-        return LegacyConfigReader.read(getConfigDirectory());
+    public ILegacyConfigProvider getLegacyConfig() {
+        return LegacyConfigReader.open(getConfigDirectory());
     }
 }
